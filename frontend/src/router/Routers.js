e52e7556eef5bbe45ed7e15c4d8f012a55194a8d
XeanN <angel.xp.pb@gmail.com>
2023-08-11 20:27:31 -0500

    second day

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -9,6 +9,7 @@ import Register from "./../pages/Register";
 import SearchResultList from './../pages/SearchResultList';
 import TourDetails from '../pages/TourDetails';
 import Tours from "./../pages/Tours";
+import ThankYou from "../pages/ThankYou";
 
 const Routers = ()=> {
     return (
@@ -19,6 +20,7 @@ const Routers = ()=> {
             <Route path='/tours/:id' element={<TourDetails/>} />
             <Route path='/login' element={<Login/>} />
             <Route path='/register' element={<Register/>} />
+            <Route path='/thank-you' element={<ThankYou/>} />
             <Route path='/tours/search' element={<SearchResultList/>} />
             
         </Routes>
